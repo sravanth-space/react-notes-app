c765ddd4a5263477b2dd1c9ae3a466764718cb86
sravanth baratam <sravanthbaratam@gmail.com>
2022-02-01 18:31:01 +0530

    Add note card is moved to the top.

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,25 +1,21 @@
-import Note from './Note';
-import AddNote from './AddNote';
+import Note from "./Note";
+import AddNote from "./AddNote";
 
-const NotesList = ({
-	notes,
-	handleAddNote,
-	handleDeleteNote,
-}) => {
-	return (
-		<div className='notes-list'>
-			{notes.map((note) => (
-				<Note
-					id={note.id}
-					key={note.id}
-					text={note.text}
-					date={note.date}
-					handleDeleteNote={handleDeleteNote}
-				/>
-			))}
-			<AddNote handleAddNote={handleAddNote} />
-		</div>
-	);
+const NotesList = ({ notes, handleAddNote, handleDeleteNote }) => {
+  return (
+    <div className="notes-list">
+      <AddNote handleAddNote={handleAddNote} />
+      {notes.map((note) => (
+        <Note
+          id={note.id}
+          key={note.id}
+          text={note.text}
+          date={note.date}
+          handleDeleteNote={handleDeleteNote}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default NotesList;
